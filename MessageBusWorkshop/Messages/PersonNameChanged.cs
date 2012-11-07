b0b825162986f3cb4cc1d28a2ea787684bd32ed5
Michael L Perry <michael@qedcode.com>
2012-11-06 19:47:06 -0600

    Added PersonNameChanged message.

---
+++ MessageBusWorkshop/Messages/PersonNameChanged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessageBusWorkshop.Messages
+{
+    public class PersonNameChanged
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
