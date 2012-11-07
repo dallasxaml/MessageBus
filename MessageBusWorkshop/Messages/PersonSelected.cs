b7afb237dc7c4eb3596c62645475deb20f38509a
Michael L Perry <michael@qedcode.com>
2012-11-06 19:10:24 -0600

    Added PersonSelected message.

---
+++ MessageBusWorkshop/Messages/PersonSelected.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessageBusWorkshop.Messages
+{
+    public class PersonSelected
+    {
+        public int PersonId { get; set; }
+    }
+}
